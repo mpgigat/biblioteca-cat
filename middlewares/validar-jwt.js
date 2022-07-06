73c46c607bd8599c6f9a15db5df951a05a7c4176
Miguel Angel <mpgigat@gmail.com>
2022-07-06 09:46:34 -0500

    borrar 6

--- middlewares/validar-jwt.js
+++ middlewares/validar-jwt.js
@@ -23,7 +23,7 @@ const validarJWT = async (req, res, next) => {
 
    // if (!token) {
         return res.status(401).json({
-            msg:`el token es: ${req}`
+            msg:`el token es: ${Object.prototype.toString.call(req)}`
         })
    // }
 
