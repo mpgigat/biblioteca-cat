a555d52d7d1243bfe594a7b2c15cb23a8e32527d
Miguel Angel <mpgigat@gmail.com>
2022-08-03 22:54:31 -0500

    subir a heroku

--- middlewares/validar_roles.js
+++ middlewares/validar_roles.js
@@ -6,9 +6,6 @@ const validarRol = (...roles) => {
         if (!(roles.includes(req.holder.rol))) {
             return res.status(401).json({ msg: `El servicio requiere uno de estos roles ${roles}` });
         }
-
-
-
         next();
     }
 }
