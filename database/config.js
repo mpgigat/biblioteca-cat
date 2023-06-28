049eec1d6d040546aff8536973c4f0efe0052055
Miguel Angel <mpgigat@gmail.com>
2023-06-28 15:40:41 -0500

    build nuevo

--- database/config.js
+++ database/config.js
@@ -2,6 +2,7 @@
 import mongoose from 'mongoose';
 const dbConnection=async()=>{
     try {
+        console.log(process.env.MONGODB_CNX);
         
         await mongoose.connect(process.env.MONGODB_CNX)
         console.log('Base de datos online');
